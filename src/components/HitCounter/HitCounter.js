13fa651ca004b9e14555fd15b61db3abb7bb9153
Joshua Comeau <joshwcomeau@gmail.com>
2018-01-21 13:23:23 -0500

    Smarter solution for digit spacing

--- src/components/HitCounter/HitCounter.js
+++ src/components/HitCounter/HitCounter.js
@@ -9,23 +9,25 @@ type Props = {
   hits: number,
   minLength: number,
   size: number,
+  backgroundColor: string,
+  padding: number,
+  digitSpacing: number,
   segmentThickness: number,
   segmentSpacing: number,
-  backgroundColor: string,
   segmentActiveColor: string,
   segmentInactiveColor: string,
-  digitSpacing: number,
 };
 
 class HitCounter extends PureComponent<Props> {
   static defaultProps = {
     minLength: 1,
-    size: 128,
-    segmentThickness: 6,
-    segmentSpacing: 1,
+    size: 40,
+    segmentThickness: 4,
+    segmentSpacing: 0.5,
     segmentActiveColor: '#00C853',
     segmentInactiveColor: 'rgba(0, 0, 0, 0.08)',
-    digitSpacing: 5,
+    digitSpacing: 3,
+    padding: 4,
   };
 
   render() {
@@ -33,38 +35,39 @@ class HitCounter extends PureComponent<Props> {
       hits,
       minLength,
       size,
+      backgroundColor,
+      padding,
       segmentThickness,
+      segmentSpacing,
       segmentActiveColor,
       segmentInactiveColor,
       digitSpacing,
     } = this.props;
 
-    const characterHeight = size;
+    const characterHeight = size - segmentThickness;
     const characterWidth = characterHeight * 0.5;
 
     const paddedValue = padStart(hits.toString(), minLength, '0');
     const individualDigits = paddedValue.split('');
 
+    const totalWidth =
+      // Total width is each number's width,
+      characterWidth * individualDigits.length +
+      // Plus spacing between them (eg 3x the spacing for 4 digits)
+      digitSpacing * (individualDigits.length - 1);
+
     return (
-      <div style={styles.wrapper(this.props)}>
+      <div style={styles.wrapper(backgroundColor, padding, totalWidth)}>
         {individualDigits.map((digit, index) => (
-          <div
-            key={index}
-            style={{
-              marginLeft: index !== 0 && digitSpacing / 2,
-              marginRight:
-                index !== individualDigits.length && digitSpacing / 2,
-            }}
-          >
-            <Digit
-              value={Number(digit)}
-              width={characterWidth}
-              height={characterHeight}
-              segmentThickness={segmentThickness}
-              segmentActiveColor={segmentActiveColor}
-              segmentInactiveColor={segmentInactiveColor}
-            />
-          </div>
+          <Digit
+            value={Number(digit)}
+            width={characterWidth}
+            height={characterHeight}
+            segmentThickness={segmentThickness}
+            segmentSpacing={segmentSpacing}
+            segmentActiveColor={segmentActiveColor}
+            segmentInactiveColor={segmentInactiveColor}
+          />
         ))}
       </div>
     );
@@ -72,9 +75,12 @@ class HitCounter extends PureComponent<Props> {
 }
 
 const styles = {
-  wrapper: (props: Props) => ({
-    display: 'flex',
-    backgroundColor: props.backgroundColor,
+  wrapper: (backgroundColor, padding, width) => ({
+    display: 'inline-flex',
+    justifyContent: 'space-between',
+    width,
+    backgroundColor,
+    padding,
   }),
 };
 
