3b402c224b40d858084e7e87b2960069970ea43a
Joshua Comeau <joshwcomeau@gmail.com>
2018-01-21 13:10:32 -0500

    Add size/color stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -10,6 +10,63 @@ storiesOf('Basic', module)
   .add('543', () => <HitCounter hits={543} />)
   .add('12345', () => <HitCounter hits={12345} />);
 
+storiesOf('Sizes', module)
+  .add('small', () => (
+    <HitCounter
+      hits={1234}
+      size={32}
+      segmentSpacing={0.5}
+      segmentThickness={4}
+      digitSpacing={2}
+    />
+  ))
+  .add('medium', () => (
+    <HitCounter
+      hits={1234}
+      size={64}
+      segmentSpacing={1}
+      segmentThickness={6}
+      digitSpacing={4}
+    />
+  ))
+  .add('large', () => (
+    <HitCounter
+      hits={1234}
+      size={128}
+      segmentSpacing={1}
+      segmentThickness={10}
+      digitSpacing={6}
+    />
+  ));
+
+storiesOf('Colors', module)
+  .add('dark', () => (
+    <HitCounter
+      hits={1234}
+      segmentActiveColor="#76FF03"
+      segmentInactiveColor="#33691E"
+      backgroundColor="#222222"
+    />
+  ))
+  .add('medium', () => (
+    <HitCounter
+      hits={1234}
+      size={64}
+      segmentSpacing={1}
+      segmentThickness={6}
+      digitSpacing={4}
+    />
+  ))
+  .add('large', () => (
+    <HitCounter
+      hits={1234}
+      size={128}
+      segmentSpacing={1}
+      segmentThickness={10}
+      digitSpacing={6}
+    />
+  ));
+
 storiesOf('Incrementing', module)
   .add('From 0', () => (
     <AutoIncrement>{val => <HitCounter hits={val} />}</AutoIncrement>
